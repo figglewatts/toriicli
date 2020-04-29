5370d570045981f1159987cd14632d84762651d0
Figglewatts <figglewatts@hotmail.co.uk>
2020-04-29 22:57:48 +0100

    Added 'new' command for creating new projects

--- toriicli/__main__.py
+++ toriicli/__main__.py
@@ -3,6 +3,7 @@ import click
 VERSION = "#{TAG_NAME}#"
 
 from .build import detect_unity
+from . import config
 
 
 @click.group()
@@ -12,8 +13,24 @@ def toriicli():
 
 
 @toriicli.command()
-def hello():
-    print(detect_unity.find_unity_executable("2017.4.30f1"))
+@click.argument("version", nargs=1, default=None, required=False)
+def unity(version):
+    """Print the path to the Unity executable. You can optionally specify a
+    specific Unity VERSION to attempt to find."""
+    exe_path = detect_unity.find_unity_executable(version)
+    if exe_path is not None:
+        print(exe_path)
+    else:
+        raise SystemExit(1)
+
+
+@toriicli.command()
+@click.argument("project_path", nargs=1, default=None, required=False)
+def new(project_path):
+    """Create a new Torii project in PROJECT_PATH. If not specified, will use
+    the current working directory as the project path."""
+    out_file_path = config.create_config(project_path)
+    print(f"Created new Torii project: {out_file_path}")
 
 
 def main():
--- toriicli/config.py
+++ toriicli/config.py
@@ -1,13 +1,27 @@
 from dataclasses import dataclass
+import os
+from os import path
+import pkg_resources
+from typing import Optional
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load, ValidationError
 import yaml
+
+EXAMPLE_CONFIG_FILE = "example_config.yml"
+CONFIG_NAME = "toriiproject.yml"
 
 
 class ToriiCliConfigSchema(Schema):
     """Marshmallow schema for app config."""
     unity_executable_path = fields.Str(required=False, missing=None)
     unity_preferred_version = fields.Str(required=False, missing=None)
+
+    @post_load
+    def make_torii_cli_config(self, data, **kwargs):
+        return ToriiCliConfig(**data)
+
+
+CONFIG_SCHEMA = ToriiCliConfigSchema()
 
 
 @dataclass
@@ -16,3 +30,64 @@ class ToriiCliConfig:
     example_config.yml in the package."""
     unity_executable_path: str
     unity_preferred_version: str
+
+
+def create_config(config_path: Optional[str]) -> str:
+    """Create a config in a given path, or the CWD if none is given.
+    Returns the path to the created config."""
+    if config_path is None:
+        config_path = os.getcwd()
+
+    os.makedirs(config_path, exist_ok=True)
+
+    # write the example config file to the config location
+    out_file_path = path.join(config_path, CONFIG_NAME)
+    with open(out_file_path, 'w') as config_file:
+        # replace any funky windows line endings that might be in there
+        example_cfg = pkg_resources.resource_string(
+            "toriicli", EXAMPLE_CONFIG_FILE).decode("utf-8").replace("\r", "")
+        config_file.write(example_cfg)
+
+    return out_file_path
+
+
+def from_yaml(config_path: str) -> Optional[ToriiCliConfig]:
+    """Load config file from given path.
+
+    If an error occurred, it will print it and return None.
+    """
+    try:
+        with open(config_path, 'r') as config_file:
+            raw_config = yaml.safe_load(config_file)
+            loaded_config = CONFIG_SCHEMA.load(raw_config)
+            return loaded_config
+    except OSError as err:
+        # if there was an error opening the file
+        print("Error opening config file: " + str(err))
+        return None
+    except yaml.YAMLError as err:
+        # if the YAML was invalid
+        print("Error in config file: " + str(err))
+        return None
+    except ValidationError as err:
+        # if the schema says it's invalid
+        _print_validation_err(err, config_path)
+        return None
+
+
+def _print_validation_err(err: ValidationError, name: str) -> None:
+    """Internal function used for printing a validation error in the Schema.
+
+    Args:
+        err (ValidationError): The error to log.
+        name (str): A human-readable identifier for the Schema data source. 
+            Like a filename.
+    """
+    # build up a string for each error
+    log_str = []
+    log_str.append(f"Error validating config '{name}':")
+    for field_name, err_msgs in err.messages.items():
+        log_str.append(f"{field_name}: {err_msgs}")
+
+    # print the joined up string
+    print(" ".join(log_str))
